6c539414d973ba04f2624b708bee0968c6e328b4
Gary Boyer <gboyer.dev@gmail.com>
2019-04-30 20:05:36 -0400

    Add HTML form elements

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+  $con = mysqli_connect("localhost", "root", "", "social");
+
+  if(mysqli_connect_errno()) {
+    echo "Failed to connect: " . mysqli_connect_errno();
+  }
+
+  $query = mysqli_query($con, "INSERT INTO test VALUES ('', 'John')" );
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>SwirlFeed</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+  <script src="main.js"></script> -->
+</head>
+<body>
+  Testing!!
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+  <title>Welcome to SwirlFeed!</title>
+</head>
+<body>
+
+  <form action="register.php">
+    <p>
+      <input type="text" name="reg_fname" placeholder="First name" required />
+    </p>
+    <p>
+      <input type="text" name="reg_lname" placeholder="Last name" required />
+    </p>
+    <p>
+      <input type="email" name="reg_email" placeholder="Email" required />
+    </p>
+    <p>
+      <input type="email" name="reg_email2" placeholder="Confirm" required />
+    </p>
+    <p>
+      <input type="password" name="reg_password" placeholder="Password" required />
+    </p>
+    <p>
+      <input type="password" name="reg_password2" placeholder="Confirm" required />
+    </p>
+    <p>
+      <button type="submit" name="register">Register</button>
+    </p>
+  </form>
+
+
+</body>
+</html>
